a0948d195127882e737d9d0b5ed2f1bbfc16af14
ethrad <asdf123f4f4@gmail.com>
2024-09-25 20:39:21 +0900

    init commit

---
+++ src/test/java/com/sparta/sparta_prepare/SpartaPrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sparta_prepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaPrepareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
