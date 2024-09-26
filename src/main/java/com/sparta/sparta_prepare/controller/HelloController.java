51a0fa2c736244daaee6ff5a21331b8a46f7d77c
ethrad <asdf123f4f4@gmail.com>
2024-09-26 19:50:39 +0900

    생애 첫 API 구현

---
+++ src/main/java/com/sparta/sparta_prepare/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.sparta.sparta_prepare.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
